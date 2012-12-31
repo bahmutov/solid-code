16cd5fe2ed6651e00c4fb8f3d3983896d5e16ad9
gleb bahmutov <gleb.bahmutov@gmail.com>
2012-12-31 18:50:40 -0500

    initial commit

---
+++ solid.js
@@ -0,0 +1,31 @@
+(function() {
+	// grab command line arguments
+	var optimist = require("optimist");
+	args = optimist.usage('JS quality metrics.\n' + 
+		'\tauthor: Gleb Bahmutov gleb.bahmutov@gmail.com\n' +
+		'\tusage: $0 <filename.js | folder with js files>')
+			.default({
+				help: false
+			})
+			.alias('h', 'help')
+			.boolean('help')
+			.argv;
+
+	function showUsageAndExit() {
+		optimist.showHelp();
+		console.log('current arguments\n', args);
+		process.exit(0);
+	}
+
+	if (!module.parent) {
+		if (args.h || args.help) {
+			showUsageAndExit();
+		}
+	}
+
+	if (args._.length === 0) {
+		console.log('missing input file|files|folder');
+		showUsageAndExit();
+	}
+
+}());
